658c96ad978970de9b187f32fe7eb64aadc4df39
Chun-Kai Wang <chunkai1312@gmail.com>
2022-07-18 20:18:44 +0800

    feat: add decorator to set oauth2 server options metadata

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -2,3 +2,4 @@ export * from './oauth2-server-authenticate.decorator';
 export * from './oauth2-server-authorize.decorator';
 export * from './oauth2-server-token.decorator';
 export * from './oauth2-server-oauth.decorator';
+export * from './oauth2-server-options.decorator';
